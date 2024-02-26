d886206290592adf241e343947eba2d5e3287a1d
celesjanette <celesaug1@gmail.com>
2024-02-25 20:34:01 -0500

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Celes Augustus on 2/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBOutlet weak var outputLabel: UILabel!
+  
+    @IBAction func tapHere(_ sender: Any) {
+        let input = txtName.text
+        let output = "Hello " + input!
+        outputLabel.text = output
+    }
+    
+override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+
